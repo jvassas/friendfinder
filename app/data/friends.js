3e22d1a6718eeb9e2a3220886fd8192a95571b58
jvassas <jakevassas@gmail.com>
2019-04-24 02:45:06 -0400

    js and questions

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,47 +2,47 @@ var friends = [
     {
         name: "Gloria Delgado-Pritchett",
         photo: "",
-        scores:[1,1,1,1,1,1,1,1,1,1]
+        scores:[3,0,2,0,0,2,5,0,1,3]
     },
     {
         name: "Phil Dunphy",
         photo: "",
-        scores:[1,1,1,1,1,1,1,1,1,1]
+        scores:[2,1,2,3,5,4,1,0,1,5]
     },
     {
         name: "Haley Dunphy",
         photo: "",
-        scores:[1,1,1,1,1,1,1,1,1,1]
+        scores:[4,1,3,0,2,2,5,0,5,4]
     },
     {
         name: "Claire Dunphy",
         photo: "",
-        scores:[1,1,1,1,1,1,1,1,1,1]
+        scores:[5,2,4,0,0,1,3,0,0,5]
     },
     {
         name: "Cameron Tucker",
         photo: "",
-        scores:[1,1,1,1,1,1,1,1,1,1]
+        scores:[3,0,4,5,2,4,4,2,2,5]
     },
     {
         name: "Manny Delgado-Pritchett",
         photo: "",
-        scores:[1,1,1,1,1,1,1,1,1,1]
+        scores:[0,3,2,1,4,2,3,5,2,3]
     },
     {
         name: "Alex Dunphy",
         photo: "",
-        scores:[1,1,1,1,1,1,1,1,1,1]
+        scores:[1,5,3,0,0,2,2,0,2,5]
     },
     {
         name: "Luke Dunphy",
         photo: "",
-        scores:[1,1,1,1,1,1,1,1,1,1]
+        scores:[0,0,2,2,1,1,0,0,5,1]
     },
     {
         name: "Mitchell Pritchett",
         photo: "",
-        scores:[1,1,1,1,1,1,1,1,1,1]
+        scores:[2,3,5,0,2,5,5,1,0,5]
     }
     
 ];
--- server.js
+++ server.js
@@ -6,11 +6,17 @@ var path = require('path');
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+//Sets up Express to handle data parsing 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
 
+//Routing
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
 
-
-
-
+//Starts server
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
 })
