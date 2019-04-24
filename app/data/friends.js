87d0843aee75f51f9c367ba4aa16ae5937a847f2
jvassas <jakevassas@gmail.com>
2019-04-23 20:19:22 -0400

    basic layout/some js

---
+++ app/data/friends.js
@@ -0,0 +1,50 @@
+var friends = [
+    {
+        name: "Gloria Delgado-Pritchett",
+        photo: "",
+        scores:[1,1,1,1,1,1,1,1,1,1]
+    },
+    {
+        name: "Phil Dunphy",
+        photo: "",
+        scores:[1,1,1,1,1,1,1,1,1,1]
+    },
+    {
+        name: "Haley Dunphy",
+        photo: "",
+        scores:[1,1,1,1,1,1,1,1,1,1]
+    },
+    {
+        name: "Claire Dunphy",
+        photo: "",
+        scores:[1,1,1,1,1,1,1,1,1,1]
+    },
+    {
+        name: "Cameron Tucker",
+        photo: "",
+        scores:[1,1,1,1,1,1,1,1,1,1]
+    },
+    {
+        name: "Manny Delgado-Pritchett",
+        photo: "",
+        scores:[1,1,1,1,1,1,1,1,1,1]
+    },
+    {
+        name: "Alex Dunphy",
+        photo: "",
+        scores:[1,1,1,1,1,1,1,1,1,1]
+    },
+    {
+        name: "Luke Dunphy",
+        photo: "",
+        scores:[1,1,1,1,1,1,1,1,1,1]
+    },
+    {
+        name: "Mitchell Pritchett",
+        photo: "",
+        scores:[1,1,1,1,1,1,1,1,1,1]
+    }
+    
+];
+
+module.exports = friends;
